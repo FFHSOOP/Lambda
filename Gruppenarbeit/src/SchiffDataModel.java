84e4b420d7d755c322f403eae2e1a9b2c180c938
ASRock <asrock@home.com>
2018-04-17 22:25:39 +0200

    Anpassung SchiffDataModel

--- Gruppenarbeit/src/SchiffDataModel.java
+++ Gruppenarbeit/src/SchiffDataModel.java
@@ -4,24 +4,31 @@ import java.util.List;
 public class SchiffDataModel {
 	List<Schiff> schiffsListe;
 	String nachricht;
+	int schlachtschiff = 1;
+	int kreuzer = 2;
+	int zerstoerer = 3;
+	int uboot = 4;
 
 	
 	SchiffDataModel(){
 		schiffsListe = new ArrayList<Schiff>();
 		
+		for (int i = 0; i < schlachtschiff; i++){
 		schiffsListe.add(new Schlachtschiff());
+		}
 		
-		schiffsListe.add(new Kreuzer());
-		schiffsListe.add(new Kreuzer());
+		for (int i = 0; i < kreuzer; i++){
+			schiffsListe.add(new Kreuzer());
+		}
 		
+		for (int i = 0; i < zerstoerer; i++){
 		schiffsListe.add(new Zerstoerer());
-		schiffsListe.add(new Zerstoerer());
-		schiffsListe.add(new Zerstoerer());
+		}
 		
+		for (int i = 0; i < uboot; i++){
 		schiffsListe.add(new UBoot());
-		schiffsListe.add(new UBoot());
-		schiffsListe.add(new UBoot());
-		schiffsListe.add(new UBoot());
+		}
+	
 	
 	}
 
